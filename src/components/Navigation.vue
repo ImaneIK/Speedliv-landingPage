a8c3389d5932bb8f7937ec59d2551c04f18b854f
ImaneIK <imaneik0910@gmail.com>
2024-08-10 16:17:04 +0100

    DEPLOY: build

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <header :class="{ 'scrolled-nav': scrolledNav }">
     <nav>
       <div class="branding">
@@ -25,29 +24,10 @@
           :icon="['fas', 'bars']"
           :class="{ 'icon-active': mobileNav }"
         ></font-awesome-icon>
-=======
-  <header :class="{'scrolled-nav' : scrolledNav}">
-    <nav>
-      <div class="branding">
-       <img src='@/assets/logo.png/' alt=""> 
-      </div>
-
-      <ul v-show="!mobile" class="navigation">
-        <li><router-link class="link" :to="{name:'home'}">Home</router-link></li>
-        <li><router-link class="link" :to="{name:'about'}">About</router-link></li>
-        <li><router-link class="link" :to="{name:''}">Portfolio</router-link></li>
-        <li><router-link class="link" :to="{name:''}">Contact</router-link></li>
-      </ul>
-
-       
-      <div class="icon">
-        <font-awesome-icon @click="toggleMobileNav" v-show="mobile"  :icon="['fas','bars']" :class="{'icon-active' : mobileNav}"></font-awesome-icon>
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
       </div>
 
       <transition name="mobile-nav">
         <ul v-show="mobileNav" class="dropdown-nav">
-<<<<<<< HEAD
           <li>
             <a class="link" href="#home"> Home </a>
             <!-- <router-link class="link" :to="{ name: 'home' }">Home</router-link> -->
@@ -62,22 +42,10 @@
       </transition>
     </nav>
   </header>
-=======
-        <li><router-link class="link" :to="{name:'home'}">Home</router-link></li>
-        <li><router-link class="link" :to="{name:'about'}">About</router-link></li>
-        <li><router-link class="link" :to="{name:''}">Portfolio</router-link></li>
-        <li><router-link class="link" :to="{name:''}">Contact</router-link></li>
-      </ul>
-      </transition>
-    </nav>
-  </header>
-
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
   name: "navigation",
   data() {
     return {
@@ -90,20 +58,6 @@ export default {
 
   created() {
     window.addEventListener("resize", this.checkScreen);
-=======
-  name:"navigation",
-  data(){
-    return{
-      scrolledNav:null,
-      mobile:null,
-      mobileNav:null,
-      windowWidth:null
-    };
-  },
-
-  created(){
-    window.addEventListener('resize', this.checkScreen);
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
     this.checkScreen();
   },
 
@@ -111,7 +65,6 @@ export default {
     window.addEventListener("scroll", this.updateScroll);
   },
 
-<<<<<<< HEAD
   methods: {
     toggleMobileNav() {
       //done
@@ -123,35 +76,17 @@ export default {
       const scrollPosition = window.scrollY;
 
       if (scrollPosition < 50) {
-=======
-  methods:{
-    toggleMobileNav(){ //done
-      this.mobileNav = !this.mobileNav;
-    },
-
-    updateScroll(){ //done
-      const scrollPosition = window.scrollY;
-
-      if(scrollPosition < 50){
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
         this.scrolledNav = true;
         return;
       }
       this.scrolledNav = false;
     },
 
-<<<<<<< HEAD
     checkScreen() {
       //done
       this.windowWidth = window.innerWidth;
 
       if (this.windowWidth <= 750) {
-=======
-    checkScreen(){ //done
-      this.windowWidth = window.innerWidth;
-
-      if(this.windowWidth <= 750){
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
         this.mobile = true;
         return;
       }
@@ -159,20 +94,13 @@ export default {
       this.mobile = false;
       this.mobileNav = false;
       return;
-<<<<<<< HEAD
     },
   },
 };
-=======
-    }
-  },
-}; 
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-<<<<<<< HEAD
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&family=Roboto:wght@300;400&display=swap");
 * {
   font-family: "Poppins", sans-serif;
@@ -314,152 +242,4 @@ li {
         width: 40px;
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
       } */
-=======
-
-  *{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-
-  header{
-   background-color: rgba(0, 0, 0, 0.8);
-    z-index: 99;
-    width: 100%;
-    position: fixed;
-    transition: 0.5s ease all;
-    color: #fff;
-    top:0;
-    left: 0;
-  }
-
-
-  nav{
-    position: relative;
-    display:flex;
-    flex-direction: row;
-    padding: 12px 0;
-    transition: 0.5s ease all;
-    width: 90%;
-    margin: 0 auto;
-    @media(min-width:1140px){
-      max-width: 1140px;
-    }
-  }
-
-   ul, .link{
-      font-weight:500;
-      color: #fff;
-      list-style: none;
-      text-decoration: none;
-    }
-
-    li{
-      text-transform: uppercase;
-      padding: 16px;
-      margin-left: 16px;
-    }
-
-    .link{
-      font-size: 14px;
-      transition: .5s ease all;
-      padding-bottom: 1px solid transparent; 
-    }
-
-     .link:hover{
-        color: #00afea;
-        border-color:#00afea ;
-      }
-
-      .branding{
-        display:flex;
-        align-items: center;
-      }
-
-      .branding img{
-        width: 50px;
-        transition: 0.5s ease all;
-      }
-
-      .navigation{
-        display: flex;
-        align-items: center;
-        flex:1;
-        justify-content: flex-end;
-      }
-
-      .icon{
-        display: flex;
-        align-items: center;
-        position: absolute;
-        top:0;
-        right: 24px;
-        height: 100%;
-      }
-
-      .icon font-awesome-icon{
-        cursor: pointer;
-        font-size: 24px;
-        transition: 0.8s ease all;
-      }
-
-      .icon-active:active,
-      .icon-active.active
-      {
-        transform: rotate(180deg);
-      }
-
-      .dropdown-nav{
-        display:flex;
-        flex-direction: column;
-        position: fixed;
-        width:100%;
-        max-width:250px;
-        height: 100%;
-        background-color: white;
-        top: 0;
-        left: 0;
-      }
-
-       .dropdown-nav li{
-          margin-left: 0;
-        }
-
-        .dropdown-nav li .link{
-          color:#000;
-        }
-
-        .mobile-nav-enter-active:active,
-        .mobile-nav-leave-active:active
-        {
-          transition: 1s leave all;
-        }
-
-        
-        .mobile-nav-enter-from,
-        .mobile-nav-leave-to{
-          transform: translateX(-250px);
-        }
-
-        
-        .mobile-nav-enter-to{
-          transform: translateX(0);
-        }
-
-      .scrolled-nav{
-        background-color: #000;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-      }
-
-      .scrolled-nav nav{
-        padding: 8px 0;
-      }
-
-      .scrolled-nav nav .branding img{
-        width: 40px;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-      }
-
-
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
     <Heading></Heading>
     <services></services>
     <why_choose_us></why_choose_us>
@@ -16,25 +15,11 @@
     <forum></forum>
     <faq></faq>
     <googleMaps></googleMaps>
-=======
-
-    <Heading></Heading>
-    
-    <howItWorks></howItWorks>
-
-    <!-- <advertisement></advertisement> -->
-
-    <mobileApp></mobileApp>
-    <forum></forum>
-
-   
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-<<<<<<< HEAD
 import Heading from "../components/heading.vue";
 import why_choose_us from "../components/why_choose_us.vue";
 // import vision from "../components/vision.vue";
@@ -68,23 +53,4 @@ export default {
     faq,
   },
 };
-=======
-import Heading from '../components/heading.vue'
-import howItWorks from '../components/how_it_works.vue'
-import mobileApp from '../components/mobile_app.vue'
-import forum from '../components/forum.vue'
-import advertisement from '../components/adv.vue'
-
-
-export default {
-  name: 'HomeView',
-  components: {
-    Heading,
-    howItWorks,
-    advertisement,
-    mobileApp,
-    forum,
-  }
-}
->>>>>>> e4d9eaa9e89cbcc4b400fd09d4ce9463f44bcccc
 </script>
